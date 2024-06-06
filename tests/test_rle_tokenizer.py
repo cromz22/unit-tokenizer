c17839c57fdeeb65ab6168b84f8d00d0f8831e92
cromz22 <tt.pfr4343@gmail.com>
2024-06-06 16:34:16 -0400

    modify test

--- tests/test_rle_tokenizer.py
+++ tests/test_rle_tokenizer.py
@@ -5,14 +5,14 @@ from tokenizers import RLETokenizer
 
 def test_encode():
     tokenizer = RLETokenizer()
-    encoded = tokenizer.encode([[0, 1, 2, 2, 3, 3, 3]])
-    assert encoded[0] == [1, 100, 1, 101, 2, 102, 3, 103]
+    encoded = tokenizer._encode([0, 1, 2, 2, 3, 3, 3])
+    assert encoded == [1, 100, 1, 101, 2, 102, 3, 103]
 
 
 def test_decode():
     tokenizer = RLETokenizer()
-    decoded = tokenizer.decode([[1, 100, 1, 101, 2, 102, 3, 103]])
-    assert decoded[0] == [0, 1, 2, 2, 3, 3, 3]
+    decoded = tokenizer._decode([1, 100, 1, 101, 2, 102, 3, 103])
+    assert decoded == [0, 1, 2, 2, 3, 3, 3]
 
 
 def test_batch_encode():
