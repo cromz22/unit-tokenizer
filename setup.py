95878a7efef0487bd34687445a4fe3c738c594c7
cromz22 <tt.pfr4343@gmail.com>
2025-02-09 16:27:31 +0900

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="unit-tokenizer",
-    version="0.1.0",
+    version="0.1.1",
     author="Shuichiro Shimizu",
     author_email="sshimizu@nlp.ist.i.kyoto-u.ac.jp",
     description="Tokenizers that operate on integer sequences.",
